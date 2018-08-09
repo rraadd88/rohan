e25aba1c07b2c775031224a8b55bf006ccb24dfd
rraadd88 <rraadd_8@hotmail.com>
2018-08-09 10:56:12 -0400

    added:ci:deploy

---
+++ test/test_install.py
@@ -0,0 +1,10 @@
+def test_install():
+    try:
+        import meld
+        # cfg=get_deps(cfg)
+        # cfg=get_genomes(cfg)
+        print(f">>> SUCCESS")
+    except:
+        print(f">>> TEST NOT SUCCESSFUL. Something's wrong with installation.")
+
+test_install()
