ea336826400682cc6762c5c74ae7700b09a07362
rraadd88 <rraadd_8@hotmail.com>
2018-10-19 11:55:40 -0400

    fix

--- rohan/dandage/io_sys.py
+++ rohan/dandage/io_sys.py
@@ -1,16 +1,17 @@
 import subprocess
 import sys
+from os.path import dirname,basename,abspath
 
 def runbashcmd(cmd,test=False,logf=None,dirs2ps=None):
-    if dirs2ps is None:
+    if not dirs2ps is None:
         import rohan
         dirs2ps={'pyp':str(subprocess.check_output('which python3'.split(' '))).replace("b'",'').replace("\\n'",''),
         'binDir':dirname(rohan.__file__)+'/bin', 
         'scriptDir':dirname(rohan.__file__)+'/bin',
         }
-    cmd = cmd.replace("$BIN", dirs2ps['binDir'])
-    cmd = cmd.replace("$PYTHON", dirs2ps['pyp'])
-    cmd = cmd.replace("$SCRIPT", dirs2ps['scriptDir'])
+        cmd = cmd.replace("$BIN", dirs2ps['binDir'])
+        cmd = cmd.replace("$PYTHON", dirs2ps['pyp'])
+        cmd = cmd.replace("$SCRIPT", dirs2ps['scriptDir'])
     if test:
         print(cmd)
     err=subprocess.call(cmd,shell=True,stdout=logf,stderr=subprocess.STDOUT)
