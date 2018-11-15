a8b942730a480900af827208875f3088797d1dbf
rraadd88 <rraadd_8@hotmail.com>
2018-11-15 14:12:28 -0500

    added: lists

--- rohan/dandage/io_sets.py
+++ rohan/dandage/io_sets.py
@@ -1,3 +1,5 @@
+## Also includes list and tuple things
+
 from functools import reduce
 import numpy as np
 
@@ -5,3 +7,12 @@ def list2intersection(l):
     return reduce(np.intersect1d, (l))
 def list2union(l):
     return np.unique(np.ravel(l))
+
+def unique(l):
+    l=[str(s) for s in l]
+    return tuple(np.unique(l))
+def tuple2str(tup,sep=' '): 
+    if isinstance(tup,tuple):
+        tup=[str(s) for s in tup]
+        tup=sep.join(list(tup))
+    return tup
