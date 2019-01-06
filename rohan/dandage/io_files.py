7d867ccfa2adfa51681affee29be88fcc09f5aff
rraadd88 <rraadd_8@hotmail.com>
2019-01-06 01:47:18 -0500

    update

--- rohan/dandage/io_files.py
+++ rohan/dandage/io_files.py
@@ -4,3 +4,32 @@ def sheetname2df_to_excel(sheetname2df,datap,):
     for sn in sheetname2df:
         sheetname2df[sn].to_excel(writer,sn)
     writer.save()
+
+from rohan.dandage.io_strs import getall_fillers    
+def fill_form(df,templatep,template_insert_line,outp,splitini,splitend,field2replace,
+           test=False):
+    """
+    """
+    template=open(templatep, 'r').read()
+    fillers=getall_fillers(template_insert_line,
+                  leftoff=-1,rightoff=1)
+    if test:
+        print(fillers)
+    insert=''
+    for index in df.index:
+        for filleri,filler in enumerate(fillers):
+            if test:
+                print(filler,str(df.loc[index,filler.replace('{','').replace('}','')]))
+            if filleri==0:
+                line=template_insert_line
+            line=line.replace(filler,str(df.loc[index,filler.replace('{','').replace('}','')]))
+        insert=insert+line
+    output=template.split(splitini)[0]+splitini+insert+splitend+template.split(splitend)[-1]
+    for field in field2replace:
+        output=output.replace(field,field2replace[field])
+    if test:        
+        print(output)
+    with open(outp,'w') as f:
+        f.write(output)
+#     return True
+    
