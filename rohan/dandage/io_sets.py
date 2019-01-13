efd45974011ca194780df92cd5e85b03d5ab8d42
rraadd88 <rraadd_8@hotmail.com>
2019-01-13 10:09:53 -0500

    update

--- rohan/dandage/io_sets.py
+++ rohan/dandage/io_sets.py
@@ -23,18 +23,22 @@ def unique(l,drop=None):
         l=[s for s in l if s!=drop]
     return tuple(np.unique(l))
 
-def unique_dropna(l): return unique(l,drop='nan')
-def merge_unique_dropna(l): return unique(list(itertools.chain(*l)),drop='nan')
-
-def tuple2str(tup,sep=' '): 
-    if isinstance(tup,tuple):
-        tup=[str(s) for s in tup]
-        tup=sep.join(list(tup))
+def tuple2str(tup,sep='; '): 
+    if not isinstance(tup,list):
+        tup=tuple(tup)
+    tup=[str(s) for s in tup]
+    tup=sep.join(list(tup))
     return tup
 
+def unique_dropna(l): return dropna(unique(l,drop='nan'))
+def unique_dropna_str(l): return tuple2str(dropna(unique(l,drop='nan')))
 
+def merge_unique_dropna(l): return dropna(unique(list(itertools.chain(*l)),drop='nan'))
 
 def rankwithlist(l,lwith,test=False):
+    """
+    rank l wrt lwith
+    """
     if not (isinstance(l,list) and isinstance(lwith,list)):
         l,lwith=list(l),list(lwith)
     from scipy.stats import rankdata
