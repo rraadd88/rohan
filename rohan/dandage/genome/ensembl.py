a0fff33a6ed48e986b0c1b6c77720af47c479323
rraadd88 <rraadd_8@hotmail.com>
2019-01-20 09:44:26 -0500

    fixes

---
+++ rohan/dandage/genome/ensembl.py
@@ -0,0 +1,47 @@
+# ensembl = pyensembl.EnsemblRelease(species=pyensembl.species.Species.register(
+# latin_name='saccharomyces_cerevisiae',
+# synonyms=['saccharomyces_cerevisiae'],
+# reference_assemblies={
+#     'R64-1-1': (92, 92),
+# }),release=92)
+
+
+def ensg2genename(id,ensembl):
+    try:
+        return ensembl.gene_name_of_gene_id(id)
+    except:
+        return np.nan
+
+def genename2ensg(name,ensembl):
+    try:
+        return gene_ids_of_gene_name('KRT8P11')
+    except:
+        return np.nan
+def enst2ensp(id,ensembl):
+    try:
+        t=ensembl.transcript_by_id(id)
+        return t.protein_id
+    except:
+        return np.nan    
+    
+def ensg2dnaseq(id,ensembl):
+    try:
+        g=ensembl.gene_by_id(id)
+        ts=g.transcripts
+        lens=[len(t.protein_sequence) if not t.protein_sequence is None else 0 for t in ts]
+        return ts[lens.index(max(lens))].id, ts[lens.index(max(lens))].protein_sequence
+    except:
+        return np.nan,np.nan     
+def enst2prtseq(id,ensembl):
+    try:
+        t=ensembl.transcript_by_id(id)
+        return t.protein_sequence
+    except:
+        return np.nan        
+def enst2rnaseq(id,ensembl):
+    try:
+        t=ensembl.transcript_by_id(id)
+        return t.coding_sequence
+    except:
+        return np.nan 
+    
