5d4235a68480ccae6c2fc90b37bf9ce0c2d910d4
rraadd88 <rraadd_8@hotmail.com>
2019-02-05 14:53:52 -0500

    update

--- rohan/dandage/io_dict.py
+++ rohan/dandage/io_dict.py
@@ -5,3 +5,10 @@ def merge_dict(d1,d2):
     for k, v in chain(d1.items(), d2.items()):
         dict3[k].append(v)
     return dict3
+
+def s2dict(s,sep=';',sep_key=':',):
+    d={}
+    for pair in s.split(sep):
+        if pair!='':
+            d[pair.split(sep_key)[0]]=pair.split(sep_key)[1]
+    return d
