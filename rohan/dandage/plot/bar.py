359c2fe96a93e92cbd22f7b8edcf8fd7dc95678e
rraadd88 <rraadd_8@hotmail.com>
2019-03-15 12:28:47 -0400

    update

--- rohan/dandage/plot/bar.py
+++ rohan/dandage/plot/bar.py
@@ -1,25 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib as mpl
+
 
 def plot_value_counts(df,col,logx=False,
                       kws_hist={'bins':10},
                       kws_bar={},
                      grid=False,
-                     axes=None,fig=None):
+                     axes=None,fig=None,
+                     hist=True):
     dplot=pd.DataFrame(df[col].value_counts()).sort_values(by=col,ascending=True)
+    figsize=(4, len(dplot)*0.4)
     if axes is None:
-        fig, axes = plt.subplots(2,1, sharex=True, 
-        #                          sharey=True,
+        if hist:
+            fig, axes = plt.subplots(2,1, sharex=True, 
                                        gridspec_kw={'height_ratios': [1, 6]},
-                                       figsize=(4, 7),
-                                )
-    _=dplot.plot.hist(ax=axes[0],legend=False,**kws_hist
-    #                   orientation="horizontal"
-                     )
-    _=dplot.plot.barh(ax=axes[1],legend=False,**kws_bar)
+                                       figsize=figsize,
+                                    )
+        else:
+            fig, axes = plt.subplots(1,1, 
+                                    figsize=figsize,
+                                    )
+    if hist:
+        _=dplot.plot.hist(ax=axes[0],legend=False,**kws_hist
+        #                   orientation="horizontal"
+                         )
+        axbar=axes[1]
+    else:
+        axbar=axes
+    _=dplot.plot.barh(ax=axbar,legend=False,**kws_bar)
+    axbar.set_xlabel('count')
+    from rohan.dandage.io_strs import linebreaker
+    axbar.set_ylabel(col.replace(' ','\n'))
     if logx:
-        for ax in axes.flat:
-            ax.set_xscale("log")
+        if hist:
+            for ax in axes.flat:
+                ax.set_xscale("log")
+                if grid:
+                    ax.set_axisbelow(False)
+        else:
+            axes.set_xscale("log")
             if grid:
-                ax.set_axisbelow(False)
+                axes.set_axisbelow(False)
+            
+               
