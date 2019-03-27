cf00270fc7c75213f59e2ae1557820fb51cd1796
rraadd88 <rraadd_8@hotmail.com>
2019-03-27 17:21:59 -0400

    update

--- rohan/dandage/db/uniprot.py
+++ rohan/dandage/db/uniprot.py
@@ -1,5 +1,45 @@
 import requests
 import pandas as pd
+def get_sequence(queries,fap=None,fmt='fasta',
+            organism_taxid=9606,
+                 test=False):
+    """
+    http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=uniprotkb&id=P14060+P26439&format=fasta&style=raw&Retrieve=Retrieve
+    https://www.uniprot.org/uniprot/?format=fasta&organism=9606&query=O75116+O75116+P35548+O14944+O14944
+    """
+    url = 'http://www.ebi.ac.uk/Tools/dbfetch/dbfetch'
+    params = {
+    'id':' '.join(queries),
+    'db':'uniprotkb',
+    'organism':organism_taxid,    
+    'format':fmt,
+    'style':'raw',
+    'Retrieve':'Retrieve',
+    }
+    response = requests.get(url, params=params)
+    if test:
+        print(response.url)
+    if response.ok:
+        if not fap is None:
+            with open(fap,'w') as f:
+                f.write(response.text)
+            return fap
+        else:
+            return response.text            
+    else:
+        print('Something went wrong ', response.status_code) 
+
+def get_sequence_batch(queries,fap,interval=1000,params_get_sequence={'organism_taxid':9606,}):
+    text=''
+    for ini,end in zip(range(0,len(queries)-1,interval),range(interval,len(queries)-1+interval,interval)):
+        print(ini,end)
+        text_=get_sequence(queries=queries[ini:end],**params_get_sequence
+                          )
+        text=f"{text}\n{text_}"
+    with open(fap,'w') as f:
+        f.write(text)
+    return fap
+
 def map_ids(queries,frm='ACC',to='ENSEMBL_PRO_ID',
             organism_taxid=9606,test=False):
     """
@@ -28,4 +68,5 @@ def map_ids_batch(queries,interval=1000,params_map_ids={'frm':'ACC','to':'ENSEMB
     for ini,end in zip(range(0,len(queries)-1,interval),range(interval,len(queries)-1+interval,interval)):
         print(ini,end)
         dgeneids=map_ids(queries=queries[ini:end],**params_map_ids)
-        range2df[ini]=dgeneids        
+        range2df[ini]=dgeneids
+    return pd.concat(range2df,axis=0).drop_duplicates()
