0b06eaa33a065911f2ef81c03bdf0f6e6f70b563
rraadd88 <rraadd_8@hotmail.com>
2019-04-23 21:24:17 -0400

    update

--- rohan/dandage/stat/norm.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-def df2zscore(df,cols=None): 
-    if cols is None:
-        cols=df.columns
-    return (df[cols] - df[cols].mean())/df[cols].std()
--- rohan/dandage/stat/transform.py
+++ rohan/dandage/stat/transform.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-def dflogcol(df,col,base=10):
+def dflogcol(df,col,base=10,pcount=0):
     if base==10:
         log =np.log10 
     elif base==2:
@@ -9,5 +9,10 @@ def dflogcol(df,col,base=10):
         log =np.log        
     else:
         ValueError(f"unseen base {base}")
-    df[f"{col} (log{base} scale)"]=df[col].apply(log).replace([np.inf, -np.inf], np.nan)
+    df[f"{col} (log{base} scale)"]=df[col].apply(lambda x : log(x+pcount)).replace([np.inf, -np.inf], np.nan)
     return df
+
+def df2zscore(df,cols=None): 
+    if cols is None:
+        cols=df.columns
+    return (df[cols] - df[cols].mean())/df[cols].std()
