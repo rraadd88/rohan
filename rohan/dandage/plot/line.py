2d6b8047dfa9fbba67876b47026f8ffee56a2e32
rraadd88 <rraadd_8@hotmail.com>
2019-04-25 07:35:45 -0400

    update

--- rohan/dandage/plot/line.py
+++ rohan/dandage/plot/line.py
@@ -4,12 +4,15 @@ import pandas as pd
 import numpy as np
 from os.path import exists, basename,dirname
 
-def plot_mean_std(df,cols=['mean','min','max','50%'],plotp=None):
+def plot_summarystats(df,cols=['mean','min','max','50%'],plotp=None):
     ax=df.loc[:,cols].plot()
     ax.fill_between(df.index, df['mean']-df['std'], df['mean']+df['std'], color='b', alpha=0.2,label='std')
     ax.legend()
     plt.tight_layout()
     if not plotp is None: 
         plt.savefig(plotp)
-    else:
-        return ax
+    return ax
+    
+def plot_mean_std(df,cols=['mean','min','max','50%'],plotp=None):
+    return plot_summarystats(df,cols=cols,plotp=plotp)
+    
