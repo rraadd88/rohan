e86629cf929a5d8c108e701ecc6714990962a768
rraadd88 <rraadd_8@hotmail.com>
2019-06-05 14:07:39 -0400

    update

--- rohan/dandage/plot/ax_.py
+++ rohan/dandage/plot/ax_.py
@@ -19,6 +19,7 @@ def grid(ax,axis=None):
         ax.xaxis.grid(color='gray', linestyle='dashed')
     return ax
 
+## egend related stuff: also includes colormaps
 def sort_legends(ax,params={}):
     handles, labels = ax.get_legend_handles_labels()
     # sort both labels and handles by labels
@@ -26,4 +27,35 @@ def sort_legends(ax,params={}):
     ax.legend(handles, labels,**params)
     return ax
 
-# def ax2items:
+def set_colorbar(fig,ax,ax_pc,label,bbox_to_anchor=(0.05, 0.5, 1, 0.45)):
+    from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+    axins = inset_axes(ax,
+                       width="5%",  # width = 5% of parent_bbox width
+                       height="50%",  # height : 50%
+                       loc=2,
+                       bbox_to_anchor=bbox_to_anchor,
+                       bbox_transform=ax.transAxes,
+                       borderpad=0,
+                       )
+    fig.colorbar(ax_pc, cax=axins, orientation="vertical",
+                 label=label,
+                )
+    return fig
+
+def set_sizelegend(fig,ax,ax_pc,sizes,label,scatter_size_scale,xoff=0.05,bbox_to_anchor=(0, 0.2, 1, 0.4)):
+    from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+    axins = inset_axes(ax,
+                       width="20%",  # width = 5% of parent_bbox width
+                       height="60%",  # height : 50%
+                       loc=2,
+                       bbox_to_anchor=bbox_to_anchor,
+                       bbox_transform=ax.transAxes,
+                       borderpad=0,
+                       )
+    axins.scatter(np.repeat(1,3),np.arange(0,3,1),s=sizes*scatter_size_scale,c='k')
+    axins.set_ylim(axins.get_ylim()[0]-xoff*2,axins.get_ylim()[1]+0.5)
+    for x,y,s in zip(np.repeat(1,3)+xoff*0.5,np.arange(0,3,1),sizes):
+        axins.text(x,y,s,va='center')
+    axins.text(axins.get_xlim()[1]+xoff,np.mean(np.arange(0,3,1)),label,rotation=90,ha='left',va='center')
+    axins.set_axis_off() 
+    return fig
