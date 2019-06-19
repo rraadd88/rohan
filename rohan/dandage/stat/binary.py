426984680469070b02608aff4cb54412953c427a
rraadd88 <rraadd_8@hotmail.com>
2019-06-19 17:27:46 -0400

    update

---
+++ rohan/dandage/stat/binary.py
@@ -0,0 +1,28 @@
+from rohan.global_import import *
+
+def log_likelihood(y_true, y_pred):
+    """
+    source: https://github.com/saezlab/protein_attenuation/blob/6c1e81af37d72ef09835ee287f63b000c7c6663c/src/protein_attenuation/utils.py
+    """
+    n = len(y_true)
+    ssr = np.power(y_true - y_pred, 2).sum()
+    var = ssr / n
+
+    l = np.longfloat(1 / (np.sqrt(2 * np.pi * var))) ** n * np.exp(-(np.power(y_true - y_pred, 2) / (2 * var)).sum())
+    ln_l = np.log(l)
+
+    return ln_l
+
+
+def f_statistic(y_true, y_pred, n, p):
+    """
+    source: https://github.com/saezlab/protein_attenuation/blob/6c1e81af37d72ef09835ee287f63b000c7c6663c/src/protein_attenuation/utils.py
+    """
+    msm = np.power(y_pred - y_true.mean(), 2).sum() / p
+    mse = np.power(y_true - y_pred, 2).sum() / (n - p - 1)
+
+    f = msm / mse
+
+    f_pval = stats.f.sf(f, p, n - p - 1)
+
+    return f, f_pval
