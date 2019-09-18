68feaea61daac86e803a349b791f244b225df911
rraadd88 <rraadd_8@hotmail.com>
2019-09-18 18:09:27 -0400

    update

--- rohan/dandage/io_dict.py
+++ rohan/dandage/io_dict.py
@@ -1,2 +1,10 @@
 from rohan.global_imports import *
 from rohan.dandage.io_sets import merge_dict,s2dict,head_dict
+
+def sort_dict(d,by_pos_in_list,out_list=False)
+    l=sorted(d.items(), key=lambda x: x[by_pos_in_list])
+    if out_list:
+        return l
+    else:
+        return dict(l)
+        
