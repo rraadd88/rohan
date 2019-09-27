48df7a8ea17dd22cc2fce970cc1023e726db53e2
rraadd88 <rraadd_8@hotmail.com>
2019-09-27 17:12:46 -0400

    update

---
+++ rohan/dandage/align/deepseq_dms.py
@@ -0,0 +1,103 @@
+from rohan.global_imports import *
+from rohan.dandage.io_seqs import *
+
+def get_nucleotide_mutmats(daligned):
+    dvalue_counts=daligned.apply(pd.value_counts)
+    dvalue_counts_ref=dvalue_counts.apply(lambda x : [np.nan if not x.name in i else x[i] for i in x.index],axis=1).apply(pd.Series)
+    dvalue_counts_ref.columns=dvalue_counts.columns
+    dvalue_counts_ref.columns.name='position, nucleotide reference'
+    dvalue_counts_mut=dvalue_counts.apply(lambda x : [x[i] if not x.name in i else np.nan for i in x.index],axis=1).apply(pd.Series)
+    dvalue_counts_mut.columns=dvalue_counts.columns
+    return dvalue_counts_mut,dvalue_counts_ref
+def plot_dntmat_mut(dntmat_mut,plotp,title,params_ax_set={},yaxis_fmt='%1.1e'):
+    from matplotlib.ticker import FormatStrFormatter
+    plt.figure(figsize=[10,1.75])
+    ax=plt.subplot()
+    dntmat_mut.T.plot(ax=ax,style='.')
+    ax.set_title(title,
+                 loc='left',
+                 ha='left')
+    ax.set(**params_ax_set)
+    ax.legend(bbox_to_anchor=[1.1,1])
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%1.1e'))                
+    plt.tight_layout()
+    savefig(plotp)
+    
+    
+def get_codon_mutations(cfg):
+    from rohan.dandage.io_strs import replacebyposition
+    dbarcodes=read_table(cfg['dbarcodesp']).sort_values(by=['Locus','Position_DMS'])
+    dbarcodes['sample name']=dbarcodes.apply(lambda x : f"{x['Locus']}_{x['Position_DMS']}",axis=1)
+    dbarcodes=dbarcodes.set_index('sample name')
+
+#     cfg['readdepth_coff_min']=3
+    refn2seq=read_fasta(cfg['referencep'])
+    refn2dn2dss={k:{'dcdis':[],
+                    'dcdinorms':[],
+                    'dntmat_mut':None,
+                    'dntmat_ref':None,
+                    'dntmat_mutaai':None,
+                    'dalignedmutaai':None} for k in dbarcodes.loc[:,'Locus'].unique()}
+    for samplen in dbarcodes.index:
+    #     if not samplen=='ABP1_CYK3_25':
+    #         continue
+        print(samplen,end=': ')
+        refn=dbarcodes.loc[samplen,'Locus']
+        aai=int(samplen.split('_')[-1])
+        ntis=aai2nti(aai)
+        label_samplen=f"{'_'.join(samplen.split('_')[:-1])}_{aai:03d}"
+
+        daligned=read_table(f"{cfg['prjd']}/{samplen}/daligned_{refn}.pqt")
+        dseqs=daligned.replace(np.nan,'N').apply(lambda x : ''.join(x.tolist()),axis=1)
+        df_=(dseqs!=refn2seq[refn])
+        refn2dn2dss[refn]['dalignedmut']=daligned.loc[df_[df_].index,:]
+        refn2dn2dss[refn]['dntmat_mut'],refn2dn2dss[refn]['dntmat_ref']=get_nucleotide_mutmats(daligned)  
+        #plot raw read depth
+        plotp=make_pathable_string(f"{cfg['prjd']}/plot/plot_qc_demupliplexed_readdepth_raw_mutation {label_samplen}.png")
+        if not exists(plotp):
+            plot_dntmat_mut(refn2dn2dss[refn]['dntmat_mut'],plotp,
+                    title=f"{label_samplen} ({((len(daligned)-len(refn2dn2dss[refn]['dalignedmut']))/len(daligned))*100:.1f}% wt reads)",
+                    params_ax_set={'ylabel':'read depth',})
+
+    #     only take the reads at the codon    
+        reg=refn2seq[refn]
+        for nti in ntis:
+            reg=replacebyposition(reg,nti,'.')
+        reg=re.compile(f"^{''.join([c if c=='.' else f'[{c}N]' for c in reg])}$")
+        df_=dseqs.apply(lambda x : bool(reg.match(x) and x!=refn2seq[refn]))
+    #     df_=dseqs.apply(lambda x : bool(re.compile(r"^...$").match(x[ntis[0]:ntis[-1]+1])) and x[ntis[0]:ntis[-1]+1]!=refn2seq[refn][ntis[0]:ntis[-1]+1])
+        refn2dn2dss[refn]['dalignedmutaai']=daligned.loc[df_[df_].index,:]
+        print(len(daligned),len(refn2dn2dss[refn]['dalignedmutaai']))
+        refn2dn2dss[refn]['dntmat_mutaai'],_=get_nucleotide_mutmats(refn2dn2dss[refn]['dalignedmutaai'])
+    #     dntmat_mutaai=dntmat_mut.copy()
+        # remove background mutations
+        refn2dn2dss[refn]['dntmat_mutaai']=refn2dn2dss[refn]['dntmat_mutaai'].applymap(lambda x : x if x>refn2dn2dss[refn]['dntmat_mutaai'].melt()['value'].quantile(0.95) or x>cfg['readdepth_coff_min'] else np.nan)
+        # get frequency
+        refn2dn2dss[refn]['dntmat_mutaai']=refn2dn2dss[refn]['dntmat_mutaai']/refn2dn2dss[refn]['dntmat_ref'].sum().mean()
+
+        #plot normalised read depth
+        plotp=make_pathable_string(f"{cfg['prjd']}/plot/plot_qc_demupliplexed_readdepth_norm_mutation {label_samplen}.png")
+        if not exists(plotp):
+            plot_dntmat_mut(refn2dn2dss[refn]['dntmat_mutaai'],plotp,
+                    title=f"{label_samplen} ({100-((len(daligned)-len(refn2dn2dss[refn]['dalignedmut']))/len(daligned))*100:.1f}% mutant reads,{((len(daligned)-len(refn2dn2dss[refn]['dalignedmut']))/len(daligned))*100:.1f}% wt reads)",
+                    params_ax_set={'ylabel':'read depth\nnormalized',})
+        # get codons counts
+        cdicols=[c for nti in ntis for c in refn2dn2dss[refn]['dalignedmutaai'] if c.startswith(f"{str(nti)} ")]
+        dcdi=refn2dn2dss[refn]['dalignedmutaai'].loc[:,cdicols].dropna().apply(lambda x : ''.join(x),axis=1).value_counts()
+        dcdi.name=f"{aai:03d} {''.join([s.split(' ')[1] for s in cdicols])}"
+        # remove background mutations
+        # get frequency
+        # make a pd.Series with the codon counts
+        dcdinorm=dcdi.apply(lambda x : x/refn2dn2dss[refn]['dntmat_ref'].sum().mean() if x>cfg['readdepth_coff_min'] else np.nan).dropna()
+        for i in list(set(mol2codes['codons']).difference(dcdi.index.tolist())):
+            dcdi[i]=np.nan    
+        for i in list(set(mol2codes['codons']).difference(dcdinorm.index.tolist())):
+            dcdinorm[i]=np.nan    
+        # concat all positions
+        # convert to aa
+        refn2dn2dss[refn]['dcdis'].append(dcdi)
+        refn2dn2dss[refn]['dcdinorms'].append(dcdinorm) 
+        del dcdi,dcdinorm
+        if cfg['test']:
+            break
+    return refn2dn2dss    
