4495e1983a2a18f5d725d94c3dcc630a6cb8b58a
rraadd88 <rraadd_8@hotmail.com>
2019-10-20 16:33:02 -0400

    update

--- rohan/dandage/align/deepseq.py
+++ rohan/dandage/align/deepseq.py
@@ -9,9 +9,9 @@ def get_coverage(aligned,reference2seq,target_ini,target_end,target=False,ref_na
     cov=pd.DataFrame()
     cov.index.name='refi'
     if target:
-        for pileupcol in aligned.pileup('amplicon', 0,len(reference2seq['amplicon']),max_depth=10000000):
-            cov.loc[pileupcol.pos, 'amplicon']=pileupcol.n
-        for pileupcol in aligned.pileup('amplicon', target_ini,target_end,max_depth=10000000):
+        for pileupcol in aligned.pileup(ref_name, 0,len(reference2seq[ref_name]),max_depth=10000000):
+            cov.loc[pileupcol.pos, ref_name]=pileupcol.n
+        for pileupcol in aligned.pileup(ref_name, target_ini,target_end,max_depth=10000000):
             cov.loc[pileupcol.pos, 'target']=pileupcol.n    
     else:
         for pileupcol in aligned.pileup(ref_name, 0,len(reference2seq[ref_name]),max_depth=10000000):
@@ -72,43 +72,46 @@ def get_aligned(dirp,method=None,test=False,quality_coff=30):
             logging.info(com)
             runbashcmd(com) 
             
-def get_daligned_target(dirp):
-    import pysam
-    from rohan.dandage.io_seqs import read_fasta
-    from rohan.dandage.io_strs import findall
-    logging.info(dirp)
-    aligned=pysam.AlignmentFile(f'{dirp}/aligned.bam', 'rb')
-    reference2seq=read_fasta(f'{dirp}/reference.fasta')
-    # target_region
-    cfg={}
-    cfg['target_ini']=findall(reference2seq['amplicon'],reference2seq['target'])[0]
-    cfg['target_end']=cfg['target_ini']+len(reference2seq['target'])
-    dcov=get_coverage(aligned,reference2seq,cfg['target_ini'],cfg['target_end'])
-    aligned_reads = aligned.fetch(contig='amplicon', 
-                                  start=cfg['target_ini'],stop=cfg['target_end'])
-    readid2seq={}
-    for read in aligned_reads:
-        if read.is_paired and not read.is_unmapped:
-            # no indels in target region
-            location_indel=get_location_first_indel(read)
-            if pd.isnull(location_indel) or location_indel>cfg['target_end']:
-                # trim to region
-                # take query from query_alignment_start to stop 
-                # and index it as reference_start to stop
-                # print(read.qstart,read.qend)
-                pos2seq=dict(zip(range(read.reference_start,read.reference_end+1),
-                        list(read.query_sequence[read.query_alignment_start:read.query_alignment_end])))
-                readid2seq[read.qname]=pd.Series(pos2seq)[list(range(cfg['target_ini'],cfg['target_end']))]
-    df=pd.concat(readid2seq,axis=1).T
-    df=df.sort_index(axis=1)
-    df.columns=[f"{i} {s}" for i,s in zip(df.columns.tolist(),list(reference2seq['target']))]
-    # save
-    to_table(dcov,f"{dirp}/dcoverage.tsv")
-    to_table(df,f"{dirp}/daligned.pqt")  
-    yaml.dump(cfg,open(f"{dirp}/cfg.yml",'w'))
-    return df            
+# def get_daligned_target(dirp):
+#     import pysam
+#     from rohan.dandage.io_seqs import read_fasta
+#     from rohan.dandage.io_strs import findall
+#     logging.info(dirp)
+#     aligned=pysam.AlignmentFile(f'{dirp}/aligned.bam', 'rb')
+#     reference2seq=read_fasta(f'{dirp}/reference.fasta')
+#     # target_region
+#     cfg={}
+#     cfg['target_ini']=findall(reference2seq['amplicon'],reference2seq['target'])[0]
+#     cfg['target_end']=cfg['target_ini']+len(reference2seq['target'])
+#     dcov=get_coverage(aligned,reference2seq,cfg['target_ini'],cfg['target_end'])
+#     aligned_reads = aligned.fetch(contig='amplicon', 
+#                                   start=cfg['target_ini'],stop=cfg['target_end'])
+#     readid2seq={}
+#     for read in aligned_reads:
+#         if read.is_paired and not read.is_unmapped:
+#             # no indels in target region
+#             location_indel=get_location_first_indel(read)
+#             if pd.isnull(location_indel) or location_indel>cfg['target_end']:
+#                 # trim to region
+#                 # take query from query_alignment_start to stop 
+#                 # and index it as reference_start to stop
+#                 # print(read.qstart,read.qend)
+#                 pos2seq=dict(zip(range(read.reference_start,read.reference_end+1),
+#                         list(read.query_sequence[read.query_alignment_start:read.query_alignment_end])))
+#                 readid2seq[read.qname]=pd.Series(pos2seq)[list(range(cfg['target_ini'],cfg['target_end']))]
+#     df=pd.concat(readid2seq,axis=1).T
+#     df=df.sort_index(axis=1)
+#     df.columns=[f"{i} {s}" for i,s in zip(df.columns.tolist(),list(reference2seq['target']))]
+#     # save
+#     to_table(dcov,f"{dirp}/dcoverage.tsv")
+#     to_table(df,f"{dirp}/daligned.pqt")  
+#     yaml.dump(cfg,open(f"{dirp}/cfg.yml",'w'))
+#     return df            
 
-def get_daligned(dirp,method):
+def get_daligned(dirp,method,target=False):
+    """
+    TODO: targetn: {refn}_target 
+    """
     import pysam
     from rohan.dandage.io_seqs import read_fasta
     from rohan.dandage.io_strs import findall
@@ -116,12 +119,16 @@ def get_daligned(dirp,method):
     aligned=pysam.AlignmentFile(f'{dirp}/aligned.bam', 'rb')
     reference2seq=read_fasta(f'{dirp}/reference.fasta')
     # target_region
-    for ref in reference2seq:      
+    for ref in [k for k in reference2seq if not 'target' in k]:
         logging.info(ref)
         cfg={}
-        cfg['target_ini']=0
-        cfg['target_end']=len(reference2seq[ref])
-        dcov=get_coverage(aligned,reference2seq,cfg['target_ini'],cfg['target_end'],target=False,ref_name=ref)
+        if not target:
+            cfg['target_ini']=0
+            cfg['target_end']=len(reference2seq[ref])
+        else:
+            cfg['target_ini']=findall(reference2seq[ref],reference2seq['target'])[0]
+            cfg['target_end']=cfg['target_ini']+len(reference2seq['target'])            
+        dcov=get_coverage(aligned,reference2seq,cfg['target_ini'],cfg['target_end'],target=target,ref_name=ref)
         aligned_reads = aligned.fetch(contig=ref, 
                                       start=cfg['target_ini'],stop=cfg['target_end'])
         readid2seq={}
