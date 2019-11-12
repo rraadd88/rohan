d4d59657800a36fd4f20d53489754d4c59dfc924
rraadd88 <rraadd_8@hotmail.com>
2019-11-12 13:57:33 -0500

    update

--- rohan/dandage/stat/cluster.py
+++ rohan/dandage/stat/cluster.py
@@ -10,22 +10,28 @@ def get_ddist_dtw(df,window):
                     ).replace(np.inf,np.nan)),colvalue_name='distance').set_index(['index','column'])
     
 # compare    
-def get_ddist(df):
+def get_ddist(df,corr=False):
 #     df=df.T
+    print(df.shape)
     method2ddists={}
+    print(f"window=",end='')
     for window in range(1,10,1):
+        print(window,end=' ')
         method2ddists[f'DTW (window={window:02d})']=get_ddist_dtw(df,window)
-    method2ddists['1-spearman']=dmap2lin((1-df.T.corr(method='spearman')),colvalue_name='distance').set_index(['index','column'])
-    method2ddists['1-pearson']=dmap2lin((1-df.T.corr(method='pearson')),colvalue_name='distance').set_index(['index','column'])
-#     method2ddists={k:dmap2lin(method2ddists[k],colvalue_name='distance').set_index(['index','column']) for k in method2ddists}
-    ddist=pd.concat(method2ddists,axis=1,
-                   )
+    if corr:
+        method2ddists['1-spearman']=dmap2lin((1-df.T.corr(method='spearman')),colvalue_name='distance').set_index(['index','column'])
+        method2ddists['1-pearson']=dmap2lin((1-df.T.corr(method='pearson')),colvalue_name='distance').set_index(['index','column'])
+
+    method2ddists={k:dmap2lin(method2ddists[k],colvalue_name='distance').set_index(['index','column']) for k in method2ddists}
+    ddist=pd.concat(method2ddists,axis=1,)
     ddist.columns=coltuples2str(ddist.columns)
 #     if not index_label is None:
 #         ddist.index.names=[f"{index_label} #{i}" for i in [1,2]]
     ddist=ddist.reset_index()
     ddist=ddist.loc[(ddist['index']!=ddist['column']),:]
     ddist['interaction id']=ddist.apply(lambda x : '--'.join(list(sorted([x['index'],x['column']]))),axis=1)
+    print(ddist.shape,end='')
     ddist=ddist.drop_duplicates(subset=['interaction id'])
+    print(ddist.shape)    
     return ddist
 
