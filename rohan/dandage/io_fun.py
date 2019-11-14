d3aa03659814b97b5582703e3a3c12deb4ec416f
rraadd88 <rraadd_8@hotmail.com>
2019-11-14 10:29:17 -0500

    update

---
+++ rohan/dandage/io_fun.py
@@ -0,0 +1,9 @@
+def f2params(f,test=False):    
+    import inspect
+    sign=inspect.signature(f)
+    params={}
+    for arg in sign.parameters:
+        argo=sign.parameters[arg]
+        params[argo.name]=argo.default
+    #     break
+    return params
