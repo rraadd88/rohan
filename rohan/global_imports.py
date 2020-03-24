e67fbd57592412b80e8f616ead6c26236c357b40
rraadd88 <rohanadandage@gmail.com>
2020-03-24 12:02:42 -0400

    update

--- rohan/global_imports.py
+++ rohan/global_imports.py
@@ -1,4 +1,3 @@
-# base
 # stats    
 import scipy as sc
 from rohan.dandage.plot.annot import *
@@ -14,14 +13,27 @@ from rohan.dandage.io_dfs import *
 from rohan.dandage.io_dict import *
 from rohan.dandage.io_strs import get_bracket
 from rohan.dandage.io_sets import *
+# paths
+pwd=abspath('.')
+prjs=['00_metaanalysis']
 # plots
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set('talk',font='Arial')
-plt.style.use('default')
-plt.rc('grid', lw=0.4,linestyle="-", color=[0.9,0.9,0.9])
+plt.style.use('ggplot')
+plt.rcParams['axes.facecolor']='none'
+plt.rcParams['axes.edgecolor']='k'
+plt.rcParams['axes.formatter.use_mathtext'] = True
+plt.rcParams['axes.formatter.limits'] = -3, 3
+plt.rcParams['axes.formatter.min_exponent'] = 3
+plt.rcParams['legend.frameon']=False
+# plt.rcParams['xtick.color']=[0.95,0.95,0.95]
+plt.rc('grid', lw=0.2,linestyle="-", color=[0.99,0.99,0.99])
 plt.rc('axes', axisbelow=True)
-
+if not any([prj in pwd for prj in prjs]):
+    plt.rcParams["font.family"] = "Monospace"
+else:
+    plt.rcParams["font.family"] = "Arial", "Monospace"
+sns.set_context('talk') # paper < notebook < talk < poster
 # always save plots 
 from rohan.dandage.figs.figure import *
 # if basename(pwd).split('_')[0].isdigit():
