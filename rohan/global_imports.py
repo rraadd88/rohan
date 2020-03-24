fa7edf4464283b0d7c0031761f0df43471e2b862
rraadd88 <rohanadandage@gmail.com>
2020-03-24 11:14:37 -0400

    update

--- rohan/global_imports.py
+++ rohan/global_imports.py
@@ -17,22 +17,10 @@ from rohan.dandage.io_sets import *
 # plots
 import matplotlib.pyplot as plt
 import seaborn as sns
-pwd=abspath('.')
-prjs=['00_metaanalysis']
-if not any([prj in pwd for prj in prjs]):
-    plt.style.use('ggplot')
-    # paper < notebook < talk < poster
-    sns.set('talk',font='Monaco')
-#     print("nogrids: plt.style.use('seaborn-white');sns.set('notebook',font='Arial')")
-    # plt.style.available
-    # import matplotlib.font_manager
-    # matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-    # stats
-else:
-    sns.set('talk',font='Arial')    
-    plt.style.use('default')
-    plt.rc('grid', lw=0.4,linestyle="-", color=[0.9,0.9,0.9])
-    plt.rc('axes', axisbelow=True)
+sns.set('talk',font='Arial')
+plt.style.use('default')
+plt.rc('grid', lw=0.4,linestyle="-", color=[0.9,0.9,0.9])
+plt.rc('axes', axisbelow=True)
 
 # always save plots 
 from rohan.dandage.figs.figure import *
