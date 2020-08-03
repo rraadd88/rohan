6aa5b0d5f579c58e0366276233088c5c3c113263
rraadd88 <rraadd_8@hotmail.com>
2020-08-03 10:24:27 -0400

    version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
 name='rohan',
 author='Rohan Dandage',
 author_email='rohanadandage@gmail.com',
-version='0.2.5',
+version='0.2.6',
 url='https://github.com/rraadd88/rohan',
 download_url='https://github.com/rraadd88/rohan/archive/master.zip',
 description='Python package for data analysis, but mostly for bioinformatics really.',
