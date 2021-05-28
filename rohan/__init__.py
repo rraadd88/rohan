491f7abeb577f3b969b14206b28526a0e8881e9c
rraadd88 <rraadd_8@hotmail.com>
2021-05-28 15:25:24 -0400

    update

--- rohan/__init__.py
+++ rohan/__init__.py
@@ -2,3 +2,8 @@ from rohan import lib
 import sys
 import rohan
 sys.modules['rohan.dandage'] = sys.modules['rohan.lib']
+from rohan import lib
+sys.modules['lib.figs'] = sys.modules['lib.figure']
+sys.modules['lib.align'] = sys.modules['lib.sequence']
+sys.modules['lib.db'] = sys.modules['lib.database']
+sys.modules['lib.ms'] = sys.modules['lib.doc']
